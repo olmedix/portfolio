1890e32023e6427aa1bc33fc2ad6fc187f45fe41
olmedix <olmedix@gmail.com>
2024-11-25 20:32:32 +0100

    creado boton menu

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import Proyectos from "./proyectos-component/Proyecto";
+import Home from "./home-component/Home";
+import Navegador from "./navegador-component/navegador";
+import SobreMi from "./sobreMi-component/SobreMi";
+import Contacto from "./contacto-component/Contacto";
+import Noticias from "./noticias-component/Noticias";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navegador />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre mi" element={<SobreMi />} />
+          <Route path="/proyectos" element={<Proyectos />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/noticias" element={<Noticias />} />
+          <Route path="*" element={<Navigate replate to="/" />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
--- src/navegador-component/navegador.jsx
+++ src/navegador-component/navegador.jsx
@@ -1,21 +1,48 @@
+import React, { useState } from "react";
 import "./navegador.css";
 import { Link } from "react-router-dom";
 
+export default function Navegador() {
+  const [menuOpen, setMenuOpen] = useState(false); // Estado para el menú
 
-export default function Navegador(){
-return(
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Alternar entre abierto y cerrado
+  };
+
+  return (
     <header className="header__container">
       <h1 className="header__title">Portfolio</h1>
-    
-    <nav className="header__nav">
+
+      {/* Botón de menú hamburguesa */}
+      <div className="menu-button" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Menú hamburguesa */}
+      {menuOpen && (
+        <nav className="hamburger-menu">
+          <ul className="nav__list">
+            <li><Link to="/" onClick={toggleMenu}>Inicio</Link></li>
+            <li><Link to="/sobre mi" onClick={toggleMenu}>Sobre mí</Link></li>
+            <li><Link to="/proyectos" onClick={toggleMenu}>Proyectos</Link></li>
+            <li><Link to="/contacto" onClick={toggleMenu}>Contacto</Link></li>
+            <li><Link to="/noticias" onClick={toggleMenu}>Noticias</Link></li>
+          </ul>
+        </nav>
+      )}
+
+      {/* Menú de navegación principal */}
+      <nav className="header__nav">
         <ul className="nav__list">
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/sobre mi">Sobre mí</Link></li>
-            <li><Link to="/proyectos">Proyectos</Link></li>
-            <li><Link to="/contacto">Contacto</Link></li>
-            <li><Link to="/noticias">Noticias</Link></li>
+          <li><Link to="/" onClick={toggleMenu}>Inicio</Link></li>
+          <li><Link to="/sobre mi" onClick={toggleMenu}>Sobre mí</Link></li>
+          <li><Link to="/proyectos" onClick={toggleMenu}>Proyectos</Link></li>
+          <li><Link to="/contacto" onClick={toggleMenu}>Contacto</Link></li>
+          <li><Link to="/noticias" onClick={toggleMenu}>Noticias</Link></li>
         </ul>
-    </nav>
+      </nav>
     </header>
-);
+  );
 }
